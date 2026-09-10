552282f04fa766d7cc61386f579a619f73de5178
prasad <awscloudfordevops@gmail.com>
2026-09-10 16:35:52 +0800

    Updated EC2 configuration

--- Terraform/Day_01/EC2.tf
+++ Terraform/Day_01/EC2.tf
@@ -7,3 +7,23 @@ resource "aws_instance" "Web_Server" {
   }
   
 }
+
+resource "aws_security_group" "Web_Server_SG" {
+  name        = "WebServerSG"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
